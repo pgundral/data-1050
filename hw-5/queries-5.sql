8ac873a7c668c4bcc129a86ac2cb7ac0dfc87ca1
Pranav Gundrala <pranavgundrala@Pranavs-MacBook-Air.local>
2025-10-21 14:37:45 -0400

    hw5

---
+++ hw-5/queries-5.sql
@@ -0,0 +1,20 @@
+-- 1. Find the names of all students who have taken or are currently taking 
+-- (in Fall' 23) Data 1050.
+
+-- 2. Find the names of students who have taken Data 1050 more than once. 
+-- (This would also include students who are currently taking Data 1050 and have also 
+-- taken it in a previous semester.)
+
+-- 3. For each department, find the total number of students who have taken 
+-- classes offered by that department in Fall, 22. Do not include any department 
+-- for which this total number is less than 2.
+
+-- 4. For each instructor,  output the name of the instructor and the total number 
+-- of students they have taught in any section of any course. 
+-- That is, the total number of students they have ever taught.
+
+-- 5. Find the id and names of instructors who have taught the least number of students.
+
+-- 6. Find the names of any classes (identified by course_id and section) being taught 
+-- in Fall '23 such that the number of students taking that class exceeds the capacity of 
+-- the classroom in which the class is taught
