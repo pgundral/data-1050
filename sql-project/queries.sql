f2b6c17bd11d8c9a99a081ac7cf34f3b05042356
Pranav Gundrala <pranavgundrala@Pranavs-MacBook-Air.local>
2025-11-06 20:42:56 -0500

    tables filled

---
+++ sql-project/queries.sql
@@ -0,0 +1,35 @@
+-- 1.
+-- Find the physicians (ssn) who have most prescribed drugs which caused alerts 
+-- (due to possible adverse interaction with a previously prescribed drug, not necessarily by the same physician).
+WITH alert_ranks AS (
+    SELECT DISTINCT p.ssn, COUNT(*)
+    FROM physicians AS p
+    INNER JOIN alerts AS a
+    ON p.ssn = a.physician_id
+    GROUP BY p.ssn
+)
+
+-- 2.
+-- Find the physicians (ssn) who have prescribed two drugs to the same patient which have adverse interactions.
+
+-- 3.
+-- Find the physicians who have prescribed most drugs supplied by company DRUGXO.
+
+-- 4.
+-- For each drug supplied by company PHARMASEE display the price (per unit of quantity) 
+-- charged by that company for that drug along with the average price charged for that drug 
+-- (by companies, not pharmacies). Note: As it happens in the data we supplied each drug is supplied 
+-- by only one company, but your query should not be based on that.
+
+-- 5.
+-- For each drug and for each pharmacy, find the percentage of the markup (per unit of quantity) for 
+-- that drug by that pharmacy.
+
+-- 6.
+-- For each drug, find the average time between when a patient was prescribed a drug and when the 
+-- prescription was filled at a pharmacy. (You will need to extract the components of a date to compute this.
+-- mySQL provides functions for doing this and the official documentation here can help provide the appropriate 
+-- functions: https://dev.mysql.com/doc/refman/8.4/en/date-and-time-functions.htmlLinks to an external site.).
+
+-- 7.
+-- For each pharmacy, find all the drugs that were prescribed to a patient and never filled at that pharmacy.
